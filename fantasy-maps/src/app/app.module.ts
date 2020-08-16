f700656bf49a5a08eb00ff0967010d476aac3e3e
Peter Vertesi <vpeter1119@gmail.com>
2020-08-16 21:51:17 +0200

    Import LeafletModule

--- fantasy-maps/src/app/app.module.ts
+++ fantasy-maps/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { MapComponent } from './map/map.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LeafletModule
   ],
   providers: [],
   bootstrap: [AppComponent]
